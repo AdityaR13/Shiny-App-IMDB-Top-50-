24c184290eae70cc4d066ba30097bb2f24e6617d
ADitya Sharma <adityamdi13@gmail.com>
2018-04-09 16:08:11 +0530

    Update imdbShiny.R

--- imdbShiny.R
+++ imdbShiny.R
@@ -11,44 +11,44 @@ imdb <- read.csv("IMDB Top 50.csv")
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
    
    # Application title
    titlePanel("IMDB movies"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a slider input for number
    sidebarLayout(
       sidebarPanel(
-        #implementing radio buttons
+        #implementing radio buttons                      # not needed in this 
         # radioButtons("p", "Select one in this dataset:",
          #            list("Rank"='a', "Rating"='b')),
         
         
-         sliderInput(inputId =  "Rank",
+         sliderInput(inputId =  "Rank",               #  inputid is used further, values is default value
                      "choose a Rank:",
                      min = 1,
                      max = 50,
                      value = 20)),
       
       
-      # Show a plot of i
+      # Main panel
       mainPanel(
-        verbatimTextOutput(outputId = "text"))))
+        verbatimTextOutput(outputId = "text"))))       # verbatim used instead of textOutput because of line breaking in output
 
 
 # Define server logic
 
 server <- function(input, output) {
-  imdbSub <- reactive({
-    a <- subset(imdb, Rank == input$Rank)
+  imdbSub <- reactive({                         # Reactive interace is needed because every input of rank filters data for that row
+    a <- subset(imdb, Rank == input$Rank)       # input rank as used earlier ( necessary )
     return(a)
   })
   
-    output$text <- renderText({
-      print(paste(" You have selected Rank:", "     ", input$Rank, "\n","\n",
-          "Movie Title is:","          " ,Title <- imdbSub()$Title, "\n","\n",
-          "Movie Rating is:","         " ,Rating <- imdbSub()$Rating, "\n","\n",
+    output$text <- renderText({                                                          # Render Text
+      print(paste(" You have selected Rank:", "     ", input$Rank, "\n","\n",            # "\n" is used for line breaking.  Used twice for double line breaking
+          "Movie Title is:","          " ,Title <- imdbSub()$Title, "\n","\n",           # imdbSub() , here using  ()  is necessary
+          "Movie Rating is:","         " ,Rating <- imdbSub()$Rating, "\n","\n",         # input spacing so output look good alligned
           "Movie Year is:","           " ,Year <- imdbSub()$Year, "\n","\n",
           "Movie Genre is:","          " ,Genre <- imdbSub()$Genre, "\n","\n",
           "Movie Actor is:", "          ",Actor <- imdbSub()$Actor, "\n","\n",
